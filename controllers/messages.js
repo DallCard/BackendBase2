9589142f23c7408ba0bf5e4096a19687be5fb99a
Dallana Cardoza Martinez <cardozadallana16@gmail.com>
2022-09-23 11:07:53 -0500

    Añadir POST, DELETE Y PUT

--- controllers/messages.js
+++ controllers/messages.js
@@ -4,5 +4,13 @@ const hiMessage =  (req, res) =>{res.send("Hola Mundo")}
 
 const byeMessage = (req, res) =>{res.send("Adios mundo")}
 
-module.exports  = {rootMessage, hiMessage, byeMessage}
+const postMessage= (req, res) =>{res.send("Mensajes Post")}
+
+const putMessage= (req, res) =>{res.send("Mensajes PUT")}
+
+const deleteMessage= (req, res) =>{res.send("Mensajes DELETE")}
+
+module.exports  = {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage}
+
+
 
--- routes/messages.js
+++ routes/messages.js
@@ -5,8 +5,10 @@ const router = Router()
 const {
     rootMessage, 
     hiMessage, 
-    byeMessage}
-    = require('../controllers/messages')
+    byeMessage,
+    postMessage,
+    putMessage,
+   deleteMessage} = require('../controllers/messages')
 
 //router.get("", () =>{})
 
@@ -16,5 +18,11 @@ router.get("/hi", hiMessage) // End Point
 //router.get("/hi"   En esta parte se le agrego el /Hi el cual al poner la ruta en el navegaor nos arrola el mensaje que se encuentra entre comilla
 
 router.get("/bye",byeMessage  ) // End Point
+
+router.post('/', postMessage)
+router.put('/', putMessage)
+router.delete('/', deleteMessage)
+
+
 
 module.exports = router
