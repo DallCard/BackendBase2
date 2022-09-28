e9a65e0b80f0873dc1ce8fdcb398d1b21e803331
Dallana Cardoza Martinez <cardozadallana16@gmail.com>
2022-09-28 11:04:10 -0500

    Añadir mensajes de error y textos

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,7 +1,20 @@
 const { request, response } = require("express")
 
 const rootMessage = (req=request, res=response) =>{
-    res.status(404).json({msg:"mensajes"})}
+    const {texto1, texto2}=req.query
+/*   if(!texto1 || !texto2){
+    res.status(400).json({msg:  "No se han enviado los parametros necesarios"
+})
+   }*/
+
+   if(!texto1){
+    res.status(200).json({msg: "Falata el parametro Texto1"})}
+
+    if(!texto2){
+        res.status(400).json({msg:   "Falta el parametro Texto2"})}
+
+        res.status(400).json({msg: texto1 + ' ' + texto2 })
+    }
 
 const hiMessage =  (req=request, res=response) =>{
     res.status(405).json({msg:"Hola Mundo"})}
